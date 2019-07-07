9481bae8d11cca95c2370642957bbca333827237
Donovan Lambert <donovan@insolus.com>
2019-07-07 21:58:50 +0200

    Initial commit (2)

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 'use strict';
 
-const warning = require('warning');
+var warning = require('warning');
 
-const warned = {};
-const warningOnce = (condition, message) => {
-    if (warned[message]) {
-        return;
-    }
+var __DEV__ = process.env.NODE_ENV !== 'production';
 
-    warning(condition, message);
-    warned[message] = !condition;
-};
+var warningOnce = function() {};
+
+if (__DEV__) {
+    var warned = {};
+    warningOnce = function(condition, format, args) {
+        if (format !== undefined && warned[format]) {
+            return;
+        }
+
+        var len = arguments.length;
+
+        args = new Array(len > 2 ? len - 2 : 0);
+        for (var key = 2; key < len; key++) {
+            args[key - 2] = arguments[key];
+        }
+
+        warning.apply(null, [condition, format].concat(args));
+        warned[message] = !condition;
+    };
+}
 
 module.exports = warningOnce;
